8f201855dfe441288eebb443792c6c983be102d1
Sam Kennedy <samk@brandwatch.com>
2018-09-28 11:50:24 +0100

    Remove redundant patterns

--- src/main/java/com/samjameskennedy/contentextractor/helpers/NodeScorer.java
+++ src/main/java/com/samjameskennedy/contentextractor/helpers/NodeScorer.java
@@ -2,32 +2,11 @@ package com.samjameskennedy.contentextractor.helpers;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Element;
 
 public class NodeScorer {
-
-    private static final Pattern UNLIKELY_CSS_CLASSES_AND_IDS =
-            Pattern.compile("com(bx|ment|munity)|dis(qus|cuss)|e(xtra|[-]?mail)|foot|"
-                    + "header|menu|re(mark|ply)|rss|sh(are|outbox)|sponsor"
-                    + "a(d|ll|gegate|rchive|ttachment)|(pag(er|ination))|popup|print|"
-                    + "login|si(debar|gn|ngle)|facebook|twitter|email");
-
-    private static final Pattern POSITIVE_CSS_CLASSES_AND_IDS =
-            Pattern.compile("(^(body|content|h?entry|main|page|post|text|blog|story|haupt))"
-                    + "|arti(cle|kel)|instapaper_body");
-
-    public static final Pattern NEGATIVE_CSS_CLASSES_AND_IDS =
-            Pattern.compile("nav($|igation)|user|com(ment|bx)|(^com-)|contact|"
-                    + "foot|masthead|(me(dia|ta))|outbrain|promo|related|scroll|(sho(utbox|pping))|"
-                    + "sidebar|sponsor|tags|tool|widget|player|disclaimer|toc|infobox|vcard|post-ratings");
-
-    private static final Pattern NEGATIVE_CSS_STYLES =
-            Pattern.compile("hidden|display: ?none|font-size: ?small");
-
-    private static final Weights WEIGHTS = new Weights();
 
     public static int scoreNode(Element element) {
         int score = calculateScore(element);
